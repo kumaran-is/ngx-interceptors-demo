4ad196a9d28a31f7ff7738cf565840b721c371ef
Kumaran Iyyasamy Srinivasan <Kumaran.X.Iyyasamy.Srinivasan.-ND@disney.com>
2020-11-22 19:59:28 -0500

    initial setup

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EMPTY, Observable } from 'rxjs';
+import { CoreConstants } from '@core/core.constants';
 import { Covid } from '@models/covid';
 import { CovidService } from '@services/covid/covid.service';
 @Component({
@@ -9,27 +10,27 @@ import { CovidService } from '@services/covid/covid.service';
 })
 export class HomeComponent implements OnInit {
 
-public columnDefs = [
-    { field: 'country', sortable: true, filter: true, resizable: true },
-    { field: 'cases', headerName: 'Total Cases', width: 120, sortable: true, resizable: true },
-    { field: 'todayCases', headerName: 'New Cases', width: 120, sortable: true, resizable: true},
-    { field: 'deaths', headerName: 'Total Deaths', width: 120, sortable: true, resizable: true },
-    { field: 'todayDeaths', headerName: 'New Deaths', width: 120, sortable: true, resizable: true},
-    { field: 'recovered', headerName: 'Total Recovered', width: 120, sortable: true, resizable: true },
-    { field: 'todayRecovered', sortable: true, width: 120, resizable: true},
-    { field: 'active', sortable: true, width: 120, resizable: true },
-    { field: 'critical', sortable: true, width: 120, resizable: true},
-    { field: 'tests', headerName: 'Total Tests', width: 120, sortable: true, resizable: true }
-];
+  public pageTitle = CoreConstants.HOME_PAGE_TITLE;
+  public rowData$: Observable<Covid[]>;
+  public columnDefs = [
+      { field: 'country', sortable: true, filter: true, resizable: true },
+      { field: 'cases', headerName: 'Total Cases', width: 120, sortable: true, resizable: true },
+      { field: 'todayCases', headerName: 'New Cases', width: 120, sortable: true, resizable: true},
+      { field: 'deaths', headerName: 'Total Deaths', width: 120, sortable: true, resizable: true },
+      { field: 'todayDeaths', headerName: 'New Deaths', width: 120, sortable: true, resizable: true},
+      { field: 'recovered', headerName: 'Total Recovered', width: 120, sortable: true, resizable: true },
+      { field: 'todayRecovered', sortable: true, width: 120, resizable: true},
+      { field: 'active', sortable: true, width: 120, resizable: true },
+      { field: 'critical', sortable: true, width: 120, resizable: true},
+      { field: 'tests', headerName: 'Total Tests', width: 120, sortable: true, resizable: true }
+  ];
 
-public rowData$: Observable<Covid[]>;
+  constructor(private covidService: CovidService) {
+    this.rowData$ = EMPTY;
+  }
 
-constructor(private covidService: CovidService) {
-  this.rowData$ = EMPTY;
-}
-
-ngOnInit(): void {
-  this.rowData$ = this.covidService.getAllCountryCovidReports();
-}
+  ngOnInit(): void {
+    this.rowData$ = this.covidService.getAllCountryCovidReports();
+  }
 
 }
